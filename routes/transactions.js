b3d95b4ce284c4f16db3227d855befba624ca3d2
Narek08 <n.avdalyan07@gmail.com>
2020-03-13 17:38:16 +0400

    Initial commit

---
+++ routes/transactions.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransactions
+} = require("../controllers/transactions");
+
+router
+  .route("/")
+  .get(getTransactions)
+  .post(addTransactions);
+
+router.route("/:id").delete(deleteTransactions);
+
+module.exports = router;
